807831cd837a3b64531e971b50fa9e873738645b
Prabhakaran <prabhakaranwebdeveloper@gmail.com>
2026-02-06 00:27:59 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import {
+  createUser,
+  getUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/users.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+
+router.post("/", createUser);
+
+router.get("/:id", getUser);
+
+router.patch("/:id", updateUser);
+
+router.delete("/:id", deleteUser);
+
+export default router;
